6f57016712cdfa82e3deede1f700bc66eecfbd32
noxcodes <samingosi97@gmail.com>
2024-12-09 14:26:45 +0300

    switching to main to check for any changes to aunthtication

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import PrivateRoute from "./utils/PrivateRoute"
+import { AuthProvider } from './context/AuthContext'
+
+import Homepage from './pages/Homepage'
+import Registerpage from './pages/Registerpage'
+import Loginpage from './pages/Loginpage'
+import Dashboard from './pages/Dashboard'
+import Navbar from './pages/Navbar'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <AuthProvider>
+        < Navbar/>
+        <Switch>
+          <PrivateRoute component={Dashboard} path="/dashboard" exact />
+          <Route component={Loginpage} path="/login" />
+          <Route component={Registerpage} path="/register" exact />
+          <Route component={Homepage} path="/" exact />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  )
 }
 
-export default App;
+export default App
