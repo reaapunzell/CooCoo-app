d2373410c8fc596b762f891f793b839964b201f9
noxcodes <samingosi97@gmail.com>
2024-12-10 22:54:56 +0300

    yet to find auth token url

--- front/src/Context/AuthContext.js
+++ front/src/Context/AuthContext.js
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
     const history = useHistory();
 
     const loginUser = async (email, password) => {
-        const response = await fetch("http://127.0.0.1:8000/api/token/", {
+        const response = await fetch("http://127.0.0.1:8000/api/auth", {
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
