69f11b056097434a1c0e5fdf24cad5a8b9066dd7
steve2700 <stevnyaru@github.com>
2025-01-10 13:02:57 +0200

    add decouple

--- server/coocoo/settings.py
+++ server/coocoo/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "coocoo.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#https://docs.djangoproject.com/en/5.0/ref/settings/
 
 #DATABASES = {
     #'default': {
